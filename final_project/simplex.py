a294fe85b02a790b21722ba442efa28bf5345d4d
Narek Ghevandiani <n.ghevandiani@gmail.com>
2019-05-03 21:05:41 +0400

    Clean up, make adding slack variables automatic

--- final_project/simplex.py
+++ final_project/simplex.py
@@ -1,68 +1,11 @@
 from heapq import nsmallest
+import numpy
 
-'''
-   Return a rectangular identity matrix with the specified diagonal entiries, possibly
-   starting in the middle.
-'''
-def identity(numRows, numCols, val=1, rowStart=0):
-   return [[(val if i == j else 0) for j in range(numCols)]
-               for i in range(rowStart, numRows)]
+def column(matrix, i):
+   return [row[i] for row in matrix]
 
-
-'''
-   standardForm: [float], [[float]], [float], [[float]], [float], [[float]], [float] -> [float], [[float]], [float]
-   Convert a linear program in general form to the standard form for the
-   simplex algorithm.  The inputs are assumed to have the correct dimensions: cost
-   is a length n list, greaterThans is an n-by-m matrix, gtThreshold is a vector
-   of length m, with the same pattern holding for the remaining inputs. No
-   dimension errors are caught, and we assume there are no unrestricted variables.
-'''
-def standardForm(cost, greaterThans=[], gtThreshold=[], lessThans=[], ltThreshold=[],
-                equalities=[], eqThreshold=[], maximization=True):
-   newVars = 0
-   numRows = 0
-   if gtThreshold != []:
-      newVars += len(gtThreshold)
-      numRows += len(gtThreshold)
-   if ltThreshold != []:
-      newVars += len(ltThreshold)
-      numRows += len(ltThreshold)
-   if eqThreshold != []:
-      numRows += len(eqThreshold)
-
-   if not maximization:
-      cost = [-x for x in cost]
-
-   if newVars == 0:
-      return cost, equalities, eqThreshold
-
-   newCost = list(cost) + [0] * newVars
-
-   constraints = []
-   threshold = []
-
-   oldConstraints = [(greaterThans, gtThreshold, -1), (lessThans, ltThreshold, 1),
-                     (equalities, eqThreshold, 0)]
-
-   offset = 0
-   for constraintList, oldThreshold, coefficient in oldConstraints:
-      constraints += [c + r for c, r in zip(constraintList,
-         identity(numRows, newVars, coefficient, offset))]
-
-      threshold += oldThreshold
-      offset += len(oldThreshold)
-
-   return newCost, constraints, threshold
-
-
-def dot(a,b):
-   return sum(x*y for x,y in zip(a,b))
-
-def column(A, j):
-   return [row[j] for row in A]
-
-def transpose(A):
-   return [column(A, j) for j in range(len(A[0]))]
+def transpose(matrix):
+   return numpy.array(matrix).transpose()
 
 def isPivotCol(col):
    return (len([c for c in col if c == 0]) == len(col) - 1) and sum(col) == 1
@@ -71,16 +14,12 @@ def variableValueForPivotColumn(tableau, column):
    pivotRow = [i for (i, x) in enumerate(column) if x == 1][0]
    return tableau[pivotRow][-1]
 
-# assume the last m columns of A are the slack variables; the initial basis is
-# the set of slack variables
 def initialTableau(c, A, b):
    tableau = [row[:] + [x] for row, x in zip(A, b)]
    tableau.append([ci for ci in c] + [0])
    return tableau
 
-
 def primalSolution(tableau):
-   # the pivot columns denote which variables are used
    columns = transpose(tableau)
    indices = [j for j, col in enumerate(columns[:-1]) if isPivotCol(col)]
    return [(colIndex, variableValueForPivotColumn(tableau, columns[colIndex]))
@@ -90,31 +29,27 @@ def primalSolution(tableau):
 def objectiveValue(tableau):
    return -(tableau[-1][-1])
 
-
 def canImprove(tableau):
    lastRow = tableau[-1]
    return any(x > 0 for x in lastRow[:-1])
 
-
-# this can be slightly faster
 def moreThanOneMin(L):
    if len(L) <= 1:
       return False
 
-   x,y = nsmallest(2, L, key=lambda x: x[1])
+   x,y = nsmallest(2, L, key = lambda x: x[1])
    return x == y
 
 
 def findPivotIndex(tableau):
-   # pick minimum positive index of the last row
    column_choices = [(i,x) for (i,x) in enumerate(tableau[-1][:-1]) if x > 0]
-   column = min(column_choices, key=lambda a: a[1])[0]
+   column = min(column_choices, key = lambda x: x[1])[0]
 
    # check if unbounded
    if all(row[column] <= 0 for row in tableau):
       raise Exception('Linear problem is unbounded.')
 
-   # check for degeneracy: more than one minimizer of the quotient
+   # check for degeneracy
    quotients = [(i, r[-1] / r[column])
       for i,r in enumerate(tableau[:-1]) if r[column] > 0]
 
@@ -126,6 +61,10 @@ def findPivotIndex(tableau):
 
    return row, column
 
+def printTableau(tableau):
+   for row in tableau:
+      print(row)
+   print()
 
 def pivotAbout(tableau, pivot):
    i,j = pivot
@@ -141,31 +80,43 @@ def pivotAbout(tableau, pivot):
 
 '''
    simplex: [float], [[float]], [float] -> [float], float
-   Solve the given standard-form linear program:
-
+   Solve the given linear program:
       max <c,x>
       s.t. Ax = b
            x >= 0
-
-   providing the optimal solution x* and the value of the objective function
 '''
+
 def simplex(c, A, b):
    tableau = initialTableau(c, A, b)
    print("Initial tableau:")
-   for row in tableau:
-      print(row)
-   print()
+   printTableau(tableau)
 
    while canImprove(tableau):
       pivot = findPivotIndex(tableau)
       print("Next pivot index is=%d,%d \n" % pivot)
       pivotAbout(tableau, pivot)
       print("Tableau after pivot:")
-      for row in tableau:
-         print(row)
-      print()
+      printTableau(tableau)
 
    return tableau, primalSolution(tableau), objectiveValue(tableau)
+
+def addSlackVariables(A, c):
+   constraint_n = len(A)
+
+   # Add slack vars
+   for i in range(constraint_n):
+      empty_array = [0 for j in range(constraint_n)]
+      empty_array[i] = 1
+      A[i] += empty_array
+
+   # Add zeros to c
+   c += [0 for j in range(constraint_n)]
+
+   return A, c
+
+def solveSimplex(c, A, b):
+   slacked_A, slacked_c = addSlackVariables(A, c)
+   return simplex(slacked_c, slacked_A, b)
 
 
 if __name__ == "__main__":
@@ -173,11 +124,6 @@ if __name__ == "__main__":
    A = [[1, 3, 2], [1, 5, 1]]
    b = [10, 8]
 
-   # add slack variables by hand
-   A[0] += [1,0]
-   A[1] += [0,1]
-   c += [0,0,0]
-
-   t, s, v = simplex(c, A, b)
+   t, s, v = solveSimplex(c, A, b)
    print(s)
    print(v)
